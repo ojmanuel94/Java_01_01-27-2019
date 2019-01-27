4fcd74265dd3f599f20f759913c78672b659a9c6
ojamnuel94 <ojmanuel94@gmail.com>
2019-01-27 14:47:40 +0800

    Second exercise

---
+++ VariableSamples.java
@@ -0,0 +1,24 @@
+public class VariableSamples {
+	
+	public static void main (String[] args) {
+		//declare a data type with variable name 
+		//result and boolean data 
+		boolean result = true;
+
+
+		//declare a data type with variable name 
+		//option and char data type 
+		char option;
+		option = 'C'; //assign 'C' to option
+
+		//declare a data type with variable name 
+		//grade, double data type and initialized 
+		//to 0.0
+		double grade = 0.0;
+
+		System.out.println("Result: " + result);
+		System.out.println("Option: " + option);
+		System.out.println("Grade: " + grade);
+	}
+
+}
